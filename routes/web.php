e2c654abcf806b9f2c7f3792691e48917c6c4137
Joao <jvictorsouto18@gmail.com>
2020-06-21 09:50:38 -0300

    commit message 11

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/login', function () {
 Route::post('/checklogin', 'LoginController@checklogin')->name('login.submit');
 
 
-Route::post('/dashboard', 'DashboardController@dashboard');
+//Route::post('/dashboard', 'DashboardController@dashboard');
 
 Route::get('/dashboard', 'DashboardController@dashboard');
 
