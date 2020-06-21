df982b8b8e79dca47fa9bb0a0be0f96a12435e1d
Joao <jvictorsouto18@gmail.com>
2020-06-21 01:13:26 -0300

    commit message 3

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -29,25 +29,77 @@ class LoginController extends Controller
     public function checklogin(Request $request){
 
         // validar o dado que vem do formulario
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|min:6'
-        ]);
+//        $request->validate([
+//            'email' => 'required|email',
+//            'password' => 'required|min:6'
+//        ]);
 
-        $user_data = array(
-            'email' => $request->get('email'),
-            'password' => $request->get('password')
+        $rules = array(
+            'email'    => 'required|email',
+            'password' => 'required|min:6'
         );
 
-        if(Auth::attempt($user_data))
-        {
-            Session::put('usuario', "logado");
-            return redirect('/dashboard');
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return Redirect::to('/')
+                ->withErrors($validator) // send back all errors to the login form
+                ->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
         }
-        else
-        {
-            return back()->with('error', 'Usuário ou Senha incorretos');
+        else {
+
+            // create our user data for the authentication
+            $userdata = array(
+
+                'password' => Input::get('password'),
+                'email' => Input::get('email')
+            );
+
+
+            // attempt to do the login
+            if (Auth::attempt($userdata, true)) {
+
+                // validation successful!
+                // redirect them to the secure section or whatever
+                // return Redirect::to('secure');
+                // for now we'll just echo success (even though echoing in a controller is bad)
+                //return Redirect::to('Home');
+                echo 'SUCCESS!';
+                echo Auth::user()->uname;
+
+                Session::put('usuario', "logado");
+                return redirect('/dashboard');
+
+            } else {
+
+                // validation not successful, send back to form
+                echo "string";
+
+
+            }
         }
+
+
+//        $user_data = array(
+//            'email' => $request->get('email'),
+//            'password' => $request->get('password')
+//        );
+//
+//
+//        var_dump(Auth::user());
+//
+//        exit();
+//
+//
+//        if(Auth::attempt($user_data,true))
+//        {
+//            Session::put('usuario', "logado");
+//            return redirect('/dashboard');
+//        }
+//        else
+//        {
+//            return back()->with('error', 'Usuário ou Senha incorretos');
+//        }
 
     }
 
