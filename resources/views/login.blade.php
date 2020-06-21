4d9b976a0135d80863491955a838c466aea53170
Joao <jvictorsouto18@gmail.com>
2020-06-21 12:21:36 -0300

    commit message 29

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link href="{{ URL::asset('css/login.css') }}" rel="stylesheet" type="text/css" >
 
 
 
