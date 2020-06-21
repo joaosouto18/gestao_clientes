469a770efba7dabced22436b6915ca32ad98471d
Joao <jvictorsouto18@gmail.com>
2020-06-21 13:26:52 -0300

    commit message 34

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/7dc068aa29.js" crossorigin="anonymous"></script>
